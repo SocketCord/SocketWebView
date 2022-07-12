e9701da50b1793de46c7eee1cfc5fe2f7392bb99
DaBluLite <dabluliteplays@gmail.com>
2022-07-12 23:40:03 +0300

    Added basic project files

---
+++ platforms/android/platform_www/inject.js
@@ -0,0 +1,28 @@
+// Start of CSS (DO NOT DELETE THIS)
+(function() {
+	var css = document.createElement("style");
+	css.innerHTML = `
+		* {
+			-webkit-tap-highlight-color: rgba(0,0,0,0);
+			-webkit-tap-highlight-color: transparent;
+		}
+		.messageAttachment-CZp8Iv > .imageWrapper-oMkQl4 {
+			width: 300px !important;
+		}
+		.messageAttachment-CZp8Iv > .imageWrapper-oMkQl4 > div > img {
+			width: 300px !important;
+			object-fit: contain !important;
+		}
+		.newMessagesBar-1hF-9G {
+			width: 100% !important;
+			border-radius: 0 !important;
+			left: 0 !important;
+		}
+		.newMessagesBar-1hF-9G > .barButtonBase-Sk2mdB {
+			height: 45px !important;
+		}
+	`;
+	document.head.appendChild(css)
+})()
+// End of CSS
+// From here on the canvas is yours, enjoy!
