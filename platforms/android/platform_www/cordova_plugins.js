4ff031d21c554a40568d1121ac55c4b75f3485b7
DaBluLite <asphatire21reborn@gmail.com>
2022-07-13 16:06:33 +0300

    Delete platforms/android directory

--- platforms/android/platform_www/cordova_plugins.js
+++
@@ -1,6 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
-  module.exports.metadata = {
-    "cordova-plugin-injectview": "1.1.1"
-  };
-});
